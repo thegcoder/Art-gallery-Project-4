e4c4097b44b12c29b8742634ee2da9f8f862f4fd
Tai Glover <taigloverslaton@gmail.com>
2019-08-14 21:20:52 -0400

    added genre.js file

---
+++ src/Components/GenreData.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const axios = require('axios');
+
+//const api = 'https://vacation-planner-api.herokuapp.com/api/';
+
+export default class ArtistData extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artWork: {}
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    axios.get(`${api}read/artWorks/${id}`)
+      .then(res => {
+        this.setState({ artWork: res.data });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      //  <h2>City: {this.state.artWork.city}</h2>
+        <div>
+          <div className="displayImage">
+            <img src={this.state.artwork.imageUrl} alt={this.state.artWork.name}/>
+          </div>
+        //  <h3>Country: {this.state.destination.country}</h3>
+          <p>{this.state.destination.description}</p>
+        </div>
+        <Link to={{ pathname:`/artWork/edit/${this.state.artWork._id}`, state: { artWork: this.state.artWork } }}>
+          <button className='search-btn'>Edit</button>
+        </Link>
+        <Link to={{ pathname:`/artWork/delete/${this.state.artWork._id}`, state: { artWork: this.state.artWork } }}>
+          <button className='search-btn'>Delete</button>
+        </Link>
+      </div>
+    )
+  }
+}
---
+++ src/Components/GenreDelete.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const axios = require('axios');
+
+//const api = 'https://vacation-planner-api.herokuapp.com/api/';
+
+export default class ArtWorkDelete extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: '',
+      imageUrl: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const data = this.state;
+
+    const history = this.props.history;
+
+    const id = this.state.id;
+
+    axios.post(`${api}delete/artWorks/${id}`, data)
+      .then(res => {
+        history.push(`/artWorks`);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    axios.get(`${api}read/artWorks/${id}`)
+      .then(res => {
+        this.setState({
+          description: res.data.description,
+          id: res.data._id,
+          imageUrl: res.data.imageUrl
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+
+  render() {
+    return (
+        <div>
+          <h2>Delete ArtWork</h2>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <div>{this.state.imageUrl}</div>
+            </div>
+            <div>
+              <div>{this.state.description}</div>
+            </div>
+            <button className='search-btn'>Submit</button>
+            <Link to={`/artWork/${this.state.id}`}><input type="button" value="Cancel" className='search-btn' /></Link>
+          </form>
+        </div>
+    );
+  }
+}
