c9b9ee873b2b755d6b430712b1e67edbb477c0a0
Tai Glover <taigloverslaton@gmail.com>
2019-08-14 20:53:01 -0400

    add div class to router

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ import './App.css';
         <div>
           <div className='nav'>
             <div className='nav-item'><Link to={'/'}><span className='nav-logo'>VP</span></Link></div>
-            <div className='nav-item'><Link to={'/destinations'}>Destinations</Link></div>
-            <div className='nav-item'><Link to={'/users'}>Users</Link></div>
+            <div className='nav-item'><Link to={'/artworks'}>ArtWorks</Link></div>
+            <div className='nav-item'><Link to={'/artists'}>Artists</Link></div>
             <div className='nav-item'><a href="https://vacation-planner-api.herokuapp.com/">API</a></div>
           </div>
           <div className='content'>
